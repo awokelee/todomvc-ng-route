3f6918c792f67c3e8b486095b29fccef8b93309b
pipixia <pipixia@pipi.com>
2017-03-08 10:13:01 +0800

    02 准备todo案例的结构

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	// 完成案例任务代码！！！
 
+	angular
+		// 创建模块
+		.module('todoApp', [])
+		// 创建控制器
+		.controller('TodoController', ['$scope', TodoController]);
+
+
+	// 控制器函数
+	function TodoController($scope) {
+		// 定义一个变量 vm，这个变量就是对 $scope 的引用！！！
+		var vm = $scope;
+
+		
+	}
 })(window);
